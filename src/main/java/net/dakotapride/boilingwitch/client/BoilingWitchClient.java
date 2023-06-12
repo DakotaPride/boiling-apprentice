10a695b012886d0c80280e9adc13bec1706a9d18
DakotaPride <rjtube13@gmail.com>
2023-06-11 22:09:31 -0500

    Model Predicates FINALLY work

--- src/main/java/net/dakotapride/boilingwitch/client/BoilingWitchClient.java
+++ src/main/java/net/dakotapride/boilingwitch/client/BoilingWitchClient.java
@@ -1,7 +1,6 @@
 package net.dakotapride.boilingwitch.client;
 
-import net.dakotapride.boilingwitch.client.geckolib.armour.VisorRenderer;
-import net.dakotapride.boilingwitch.common.BoilingWitchMod;
+import net.dakotapride.boilingwitch.client.geckolib.armour.MaskOfPurityRenderer;
 import net.dakotapride.boilingwitch.common.register.content.BlockRegister;
 import net.dakotapride.boilingwitch.common.register.content.ItemRegister;
 import net.dakotapride.boilingwitch.common.register.content.ScreenRegister;
@@ -9,6 +8,7 @@ import net.dakotapride.boilingwitch.common.screen.screen.GlyphEnhancementScreen;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.client.gui.screen.ingame.HandledScreens;
+import net.minecraft.client.item.ModelPredicateProviderRegistry;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.util.Identifier;
 import software.bernie.geckolib3.renderers.geo.GeoArmorRenderer;
@@ -32,10 +32,21 @@ public class BoilingWitchClient implements ClientModInitializer {
 
 		HandledScreens.register(ScreenRegister.ENHANCEMENT_HANDLER, GlyphEnhancementScreen::new);
 
+		GeoArmorRenderer.registerArmorRenderer(new MaskOfPurityRenderer(), ItemRegister.MASK_OF_PURITY);
 
-		RenderLayer.getEntityTranslucent(new Identifier(BoilingWitchMod.MOD_ID, "textures/models/armor/visor.png"));
+		ModelPredicateProviderRegistry.register(ItemRegister.OCARINA,
+				new Identifier("curse"),
+				(stack, world, entity, seed) -> {
 
-		GeoArmorRenderer.registerArmorRenderer(new VisorRenderer(), ItemRegister.MASK_OF_PURITY);
+			if (entity == null) {
+				return 0.0F;
+			} else if (stack.getNbt() != null) {
+				return stack.getNbt().contains("curse") ? stack.getNbt().getFloat("curse") : 0.0F;
+			} else {
+				return 1.0F;
+			}
+
+		});
 
 	}
 }
